fe30a6500b6129fc92f896d3124360c0274f0570
Fabian Gebhart <16943048+fgebhart@users.noreply.github.com>
2023-02-04 17:51:40 +0100

    ensure 2nd matrix has same shape

--- keyboards/crkbd/rev1/rev1.h
+++ keyboards/crkbd/rev1/rev1.h
@@ -29,14 +29,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                       K36, K37, K38, K39, K40, K41 \
   ) \
   { \
-    { K00, K01, K02, K03, K04, K05 }, \
-    { K12, K13, K14, K15, K16, K17 }, \
-    { K24, K25, K26, K27, K28, K29 }, \
-    { KC_NO, KC_NO, KC_NO, K36, K37, K38 }, \
-    { K11, K10, K09, K08, K07, K06 }, \
-    { K23, K22, K21, K20, K19, K18 }, \
-    { K35, K34, K33, K32, K31, K30 }, \
-    { KC_NO, KC_NO, KC_NO, K41, K40, K39 } \
+    { K00, K01, K02, K03, K04, K05, K11, K10, K09, K08, K07, K06 }, \
+    { K12, K13, K14, K15, K16, K17, K23, K22, K21, K20, K19, K18 }, \
+    { K24, K25, K26, K27, K28, K29, K35, K34, K33, K32, K31, K30 }, \
+    { KC_NO, KC_NO, KC_NO, K36, K37, K38, KC_NO, KC_NO, KC_NO, K41, K40, K39 } \
   }
 
 #define LAYOUT_split_3x5_3( \

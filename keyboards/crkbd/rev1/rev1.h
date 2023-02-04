74e66dffecf5b187e1f05190f3213ca178ec264f
Fabian Gebhart <16943048+fgebhart@users.noreply.github.com>
2023-02-04 17:24:51 +0100

    my changes

--- keyboards/crkbd/rev1/rev1.h
+++ keyboards/crkbd/rev1/rev1.h
@@ -23,37 +23,37 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // clang-format off
 #define LAYOUT_split_3x6_3( \
-  L00, L01, L02, L03, L04, L05,           R00, R01, R02, R03, R04, R05, \
-  L10, L11, L12, L13, L14, L15,           R10, R11, R12, R13, R14, R15, \
-  L20, L21, L22, L23, L24, L25,           R20, R21, R22, R23, R24, R25, \
-                      L30, L31, L32, R30, R31, R32 \
+  K00, K01, K02, K03, K04, K05,           K06, K07, K08, K09, K10, K11, \
+  K12, K13, K14, K15, K16, K17,           K18, K19, K20, K21, K22, K23, \
+  K24, K25, K26, K27, K28, K29,           K30, K31, K32, K33, K34, K35, \
+                      K36, K37, K38, K39, K40, K41 \
   ) \
   { \
-    { L00, L01, L02, L03, L04, L05 }, \
-    { L10, L11, L12, L13, L14, L15 }, \
-    { L20, L21, L22, L23, L24, L25 }, \
-    { KC_NO, KC_NO, KC_NO, L30, L31, L32 }, \
-    { R05, R04, R03, R02, R01, R00 }, \
-    { R15, R14, R13, R12, R11, R10 }, \
-    { R25, R24, R23, R22, R21, R20 }, \
-    { KC_NO, KC_NO, KC_NO, R32, R31, R30 } \
+    { K00, K01, K02, K03, K04, K05 }, \
+    { K12, K13, K14, K15, K16, K17 }, \
+    { K24, K25, K26, K27, K28, K29 }, \
+    { KC_NO, KC_NO, KC_NO, K36, K37, K38 }, \
+    { K11, K10, K09, K08, K07, K06 }, \
+    { K23, K22, K21, K20, K19, K18 }, \
+    { K35, K34, K33, K32, K31, K30 }, \
+    { KC_NO, KC_NO, KC_NO, K41, K40, K39 } \
   }
 
 #define LAYOUT_split_3x5_3( \
-  L00, L01, L02, L03, L04,           R00, R01, R02, R03, R04, \
-  L10, L11, L12, L13, L14,           R10, R11, R12, R13, R14, \
-  L20, L21, L22, L23, L24,           R20, R21, R22, R23, R24, \
-                 L30, L31, L32, R30, R31, R32 \
+  K00, K01, K02, K03, K04,           K06, K07, K08, K09, K10, \
+  K12, K13, K14, K15, K16,           K18, K19, K20, K21, K22, \
+  K24, K25, K26, K27, K28,           K30, K31, K32, K33, K34, \
+                 K36, K37, K38, K39, K40, K41 \
   ) \
   { \
-    { KC_NO, L00, L01, L02, L03, L04 }, \
-    { KC_NO, L10, L11, L12, L13, L14 }, \
-    { KC_NO, L20, L21, L22, L23, L24 }, \
-    { KC_NO, KC_NO, KC_NO, L30, L31, L32 }, \
-    { KC_NO, R04, R03, R02, R01, R00 }, \
-    { KC_NO, R14, R13, R12, R11, R10 }, \
-    { KC_NO, R24, R23, R22, R21, R20 }, \
-    { KC_NO, KC_NO, KC_NO, R32, R31, R30 } \
+    { KC_NO, K00, K01, K02, K03, K04 }, \
+    { KC_NO, K12, K13, K14, K15, K16 }, \
+    { KC_NO, K24, K25, K26, K27, K28 }, \
+    { KC_NO, KC_NO, KC_NO, K36, K37, K38 }, \
+    { KC_NO, K10, K09, K08, K07, K06 }, \
+    { KC_NO, K22, K21, K20, K19, K18 }, \
+    { KC_NO, K34, K33, K32, K31, K30 }, \
+    { KC_NO, KC_NO, KC_NO, K41, K40, K39 } \
   }
 
 
